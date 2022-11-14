613360a90edc6143cb2b9ec066e1ce01559a506b
dynajosh <oriakhi78@gmail.com>
2022-11-14 12:51:16 +0100

    wrapped up user profile

--- apis/v1/auth_route.py
+++ apis/v1/auth_route.py
@@ -2,12 +2,13 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from db.repository.auth import get_user
 from core.hashing import Hasher
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from schemas import Token
 from core.config import settings
 from datetime import timedelta
 from db.session import get_db
 from core.security import create_access_token
+from jose import jwt, JWTError
 
 
 router = APIRouter()
@@ -35,3 +36,23 @@ def login_for_Access_token(form_data: OAuth2PasswordRequestForm=Depends(), db:Se
         data={"sub": user.email}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type":"bearer"}
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
+
+def get_current_user_from_token(token: str = Depends(oauth2_scheme),db: Session=Depends(get_db)): 
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        username: str = payload.get("sub")
+        print("username/email extracted is ",username)
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username=username,db=db)
+    if user is None:
+        raise credentials_exception
+    return user
--- apis/v1/student_route.py
+++ apis/v1/student_route.py
@@ -5,6 +5,8 @@ from schemas import StudentCreate, ShowStudent
 from db.session import get_db
 from db.repository.students import create_new_student, retrieve_student, fetch_all_students, update_student_data, delete_a_student
 from typing import List
+from apis.v1.auth_route import get_current_user_from_token
+from db.models import User
 
 router = APIRouter()
 
@@ -34,9 +36,14 @@ def update_student(id:int, student:StudentCreate, db:Session=Depends(get_db)):
         return {"msg":"Successfully updated."}
 
 @router.delete("delete/{id}")
-def delete_student(id:int, db:Session=Depends(get_db)):
-    current_user = 1
-    student = delete_a_student(id=id, db=db, teacher_id=current_user)
+def delete_student(id:int, db:Session=Depends(get_db), current_user:User=Depends(get_current_user_from_token)):
+    student = retrieve_student(id=id, db=db)
+    print(student)
+    if student.teacher_id == current_user:
+        delete_a_student(id=id, db=db, current_user=current_user)
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You are not permitted to delete this student because you didn't create them.")
+    # student = delete_a_student(id=id, db=db, current_user=current_user)
 
     if not student:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Student with ID {id} not found")
